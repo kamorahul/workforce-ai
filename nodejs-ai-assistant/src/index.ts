1f0e0b020f8e5cb583346f408a3de4a1daf49d7e
Rahul Kamboj <kamo.rahul@gmail.com>
2025-04-09 11:52:23 +0530

    added webhook changes

--- nodejs-ai-assistant/src/index.ts
+++ nodejs-ai-assistant/src/index.ts
@@ -102,6 +102,11 @@ app.post('/webhook', async (req, res): Promise<void> => {
   const agent = await createAgent(user as User, channel.type, channel.id);
   console.log("Body:", agent);
 
+  if(user.id==='kai') {
+    res.json(req.body);
+    return;
+
+  }
   await agent.init("asst_Q8vD9YOGcO3es62kFjeVZI5L");
   // if (summaryChannel) {
     agent.handleMessage(
@@ -112,7 +117,7 @@ app.post('/webhook', async (req, res): Promise<void> => {
   //       `Generate today's Summary for ${user.name} for groupId ${channelIdUpdated}. Don't mention groupId in the result.`,
   //   );
   // }
-  res.json(req.body);
+
 });
 
 // Start the Express server
